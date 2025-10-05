7a877097754ea0c66bb5b3d33a88d4bcb5193b52
nemcodes <florenceannabellobiakor@gmail.com>
2025-10-05 22:33:48 +0100

    URL updated in authstore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth"; // Base URL for the backend API
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth"; // Base URL for the backend API
 axios.defaults.withCredentials = true; // Enable sending cookies with requests
 
 export const useAuthStore = create((set) => ({
