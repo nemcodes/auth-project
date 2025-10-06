73d6d82936412fa4b60688cd5bc43ad8d2f385d7
nemcodes <florenceannabellobiakor@gmail.com>
2025-10-06 16:23:47 +0100

    Prepare backend for serverless on Vercel

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
+
+let isConnected;
+
 export const connectDB = async () => {
+  if (isConnected) {
+    console.log("=> Using existing database connection");
+    return;
+  }
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = conn.connections[0].readyState;
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1);
+    throw new Error("DB connection failed");
   }
 };
+
 export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import path, { join } from "path";
 import { fileURLToPath } from "url";
+const serverless = require("serverless-http");
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
@@ -42,5 +43,7 @@ app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = serverless(app);
 
 export default app;
